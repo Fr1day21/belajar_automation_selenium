bf3edaeea207e1d4b1f3d5a1e4d42eec97bc4da6
Rhesna Aridlo Setiawa <rhesna1922@gmail.com>
2024-10-07 13:40:09 +0700

    add link test and validation

--- src/main/java/com/demoqa/pages/elements/ElementsPage.java
+++ src/main/java/com/demoqa/pages/elements/ElementsPage.java
@@ -6,11 +6,17 @@ import org.openqa.selenium.By;
 public class ElementsPage extends HomePage {
 
     private final By webTablesMenuItem = By.xpath("//span[text()='Web Tables']");
+    private final By linksMenuItem =  By.xpath("//li[@id='item-5']//span[text()='Links']");
 
 
     public WebTablePage clickWebTables(){
         click(webTablesMenuItem);
         return new WebTablePage();
+    }
+
+    public LinksPage clickLinks(){
+        click(linksMenuItem);
+        return new LinksPage();
     }
 
 
