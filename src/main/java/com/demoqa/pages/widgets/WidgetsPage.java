e9c9e8813d46f90e12d5a5616d354da880baa3e2
Rhesna Aridlo Setiawa <rhesna1922@gmail.com>
2024-10-08 17:35:41 +0700

    add new access to Date Picker

--- src/main/java/com/demoqa/pages/widgets/WidgetsPage.java
+++ src/main/java/com/demoqa/pages/widgets/WidgetsPage.java
@@ -8,11 +8,18 @@ import static utilities.JavaScriptUtility.scrollToElementJS;
 public class WidgetsPage extends HomePage {
 
     private final By selectMenuItem = By.xpath("//span[text()='Select Menu']");
+    private final By datePickerMenuItem = By.xpath("//span[text()='Date Picker']");
 
     public SelectMenuPage clickSelectMenu(){
         scrollToElementJS(selectMenuItem);
         click(selectMenuItem);
         return new SelectMenuPage();
+    }
+
+    public DatePickerMenuPage clickDatePicker(){
+        scrollToElementJS(datePickerMenuItem);
+        click(datePickerMenuItem);
+        return new DatePickerMenuPage();
     }
 
 
