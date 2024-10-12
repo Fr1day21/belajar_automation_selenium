26f76c0592658fecfe7d7996ba4eba56d75ea57e
Rhesna Aridlo Setiawa <rhesna1922@gmail.com>
2024-10-12 16:12:39 +0700

    add test for frame small box

--- src/test/java/part3_4/com/demoqa/tests/part4/frames/FramesTest.java
+++ src/test/java/part3_4/com/demoqa/tests/part4/frames/FramesTest.java
@@ -21,6 +21,19 @@ public class FramesTest extends BaseTest {
 
 
 
+    @Test
+    public void  testFramesSmallBox(){
+        var framesPage = homePage.goToAlertsFramesWindowsCard().clickFrames();
+        String actualSmallBoxText = framesPage.getTextInSmallFrame();
+        String expectedSmallBoxText = "This is a sample page";
+        Assert.assertEquals(actualSmallBoxText, expectedSmallBoxText, "\n Actual & Expected Text Do Not Match \n");
+        String actualHeaderText = framesPage.getHeaderFramesText();
+        String expectHeaderText = "Frames";
+        Assert.assertEquals(actualHeaderText, expectHeaderText, "\n Actual & Expected Header Text Do Not Match\n");
+    }
+
+
+
 
 
 
