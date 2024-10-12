636a9c1a2c240c9bf13a77a0601a923d9ab0566f
Rhesna Aridlo Setiawa <rhesna1922@gmail.com>
2024-10-12 12:01:31 +0700

    add do an action to menu frames

--- src/main/java/com/demoqa/pages/alerts_frames_windows/Alert_Frames_WindowsPage.java
+++ src/main/java/com/demoqa/pages/alerts_frames_windows/Alert_Frames_WindowsPage.java
@@ -9,6 +9,7 @@ public class Alert_Frames_WindowsPage extends HomePage {
 
     private final By modalDialogsMenuItem = By.xpath("//span[text()='Modal Dialogs']");
     private final By alertsMenuItem = By.xpath("//span[text()='Alerts']");
+    private final By framesMenuItem = By.xpath("//span[text()='Frames']");
 
 
     public AlertsPage clickAlerts(){
@@ -22,6 +23,12 @@ public class Alert_Frames_WindowsPage extends HomePage {
         scrollToElementJS(modalDialogsMenuItem);
         click(modalDialogsMenuItem);
         return new ModelDialogPage();
+    }
+
+    public FramesPage clickFrames(){
+        scrollToElementJS(framesMenuItem);
+        click(framesMenuItem);
+        return new FramesPage();
     }
 
 
