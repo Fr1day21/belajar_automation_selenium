0ec591c358ef97bd375235aeaef599bf24b3b18e
Rhesna Aridlo Setiawa <rhesna1922@gmail.com>
2024-10-12 16:11:46 +0700

    add do an action switch to frame index and element

--- src/main/java/com/demoqa/pages/alerts_frames_windows/FramesPage.java
+++ src/main/java/com/demoqa/pages/alerts_frames_windows/FramesPage.java
@@ -6,9 +6,10 @@ import static utilities.SwitchToUtility.*;
 
 public class FramesPage extends Alert_Frames_WindowsPage{
 
-    private final By textInframe = By.id("sampleHeading");
+    private final By textIframe = By.id("sampleHeading");
     private final String iFrameBigBox = "frame1";
     private final By headerFramesText = By.xpath("//h1[text()='Frames']");
+    private final By iFrameSmallBox = By.xpath("//div[@id='frame2Wrapper']//iframe");
 
     private void switchToBigBox(){
         switchToFramesString(iFrameBigBox);
@@ -22,10 +23,25 @@ public class FramesPage extends Alert_Frames_WindowsPage{
 
     public String getTextInBigFrame(){
         switchToBigBox();
-        String bigFrameText = find(textInframe).getText();
+        String bigFrameText = find(textIframe).getText();
         System.out.println("Big Frame Text: " + bigFrameText);
         switchToDefaultContent();
         return bigFrameText;
+    }
+
+    private void switchToSmallBox(){
+//        switchToFrameIndex(3);
+        switchToFrameElement(find(iFrameSmallBox));
+    }
+
+
+    public String getTextInSmallFrame(){
+        delay(2000);
+        switchToSmallBox();
+        String smallFrameText = find(textIframe).getText();
+        System.out.println("Small Frame Text : " + smallFrameText);
+        switchToDefaultContent();
+        return smallFrameText;
     }
 
 
