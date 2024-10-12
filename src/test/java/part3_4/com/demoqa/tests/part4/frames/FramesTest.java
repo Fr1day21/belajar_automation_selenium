d110ba531829c9375f2ecff675a1d42f6c4d37bf
Rhesna Aridlo Setiawa <rhesna1922@gmail.com>
2024-10-12 11:55:58 +0700

    add some test for switch to frames using string

---
+++ src/test/java/part3_4/com/demoqa/tests/part4/frames/FramesTest.java
@@ -0,0 +1,28 @@
+package part3_4.com.demoqa.tests.part4.frames;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import part3_4.com.demoqa.base.BaseTest;
+
+public class FramesTest extends BaseTest {
+
+
+    @Test
+    public void testFramesBigBox(){
+        var framesPage = homePage.goToAlertsFramesWindowsCard().clickFrames();
+        String actualBigBoxTest = framesPage.getTextInBigFrame();
+        String expectedBigBoxText = "This is a sample page";
+        Assert.assertEquals(actualBigBoxTest, expectedBigBoxText, "\n Actual & Expected Text Do Not Match \n");
+
+        String actualHeaderText = framesPage.getHeaderFramesText();
+        String expectHeaderText = "Frames";
+        Assert.assertEquals(actualHeaderText, expectHeaderText, "\n Actual & Expected Header Text Do Not Match\n");
+    }
+
+
+
+
+
+
+
+}
