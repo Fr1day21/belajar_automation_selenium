b49c034827ceb18e0be3ccd4dfa4bb43cce988ee
Rhesna Aridlo Setiawa <rhesna1922@gmail.com>
2024-10-13 12:16:02 +0700

    add some do an action to dynamic properties menu

--- src/main/java/com/demoqa/pages/elements/ElementsPage.java
+++ src/main/java/com/demoqa/pages/elements/ElementsPage.java
@@ -3,10 +3,13 @@ package com.demoqa.pages.elements;
 import com.demoqa.pages.HomePage;
 import org.openqa.selenium.By;
 
+import static utilities.JavaScriptUtility.scrollToElementJS;
+
 public class ElementsPage extends HomePage {
 
     private final By webTablesMenuItem = By.xpath("//span[text()='Web Tables']");
     private final By linksMenuItem =  By.xpath("//li[@id='item-5']//span[text()='Links']");
+    private final By dynamicPropertiesMenuItem = By.xpath("//span[text()='Dynamic Properties']");
 
 
     public WebTablePage clickWebTables(){
@@ -17,6 +20,12 @@ public class ElementsPage extends HomePage {
     public LinksPage clickLinks(){
         click(linksMenuItem);
         return new LinksPage();
+    }
+
+    public DynamicPropertiesPage clickDynamicProperties(){
+        scrollToElementJS(dynamicPropertiesMenuItem);
+        click(dynamicPropertiesMenuItem);
+        return  new DynamicPropertiesPage();
     }
 
 
