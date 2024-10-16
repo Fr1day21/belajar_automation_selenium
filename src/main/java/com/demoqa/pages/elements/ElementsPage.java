dbf5dd8ec3f06777a67ffdcbc334d3155548a8e6
Rhesna Aridlo Setiawa <rhesna1922@gmail.com>
2024-10-16 11:09:24 +0700

    add do an action to text box menu

--- src/main/java/com/demoqa/pages/elements/ElementsPage.java
+++ src/main/java/com/demoqa/pages/elements/ElementsPage.java
@@ -10,6 +10,7 @@ public class ElementsPage extends HomePage {
     private final By webTablesMenuItem = By.xpath("//span[text()='Web Tables']");
     private final By linksMenuItem =  By.xpath("//li[@id='item-5']//span[text()='Links']");
     private final By dynamicPropertiesMenuItem = By.xpath("//span[text()='Dynamic Properties']");
+    private final By textBoxMenuItem = By.xpath("//span[text()='Text Box']");
 
 
     public WebTablePage clickWebTables(){
@@ -26,6 +27,12 @@ public class ElementsPage extends HomePage {
         scrollToElementJS(dynamicPropertiesMenuItem);
         click(dynamicPropertiesMenuItem);
         return  new DynamicPropertiesPage();
+    }
+
+    public TextBoxPage clickTextBox(){
+        scrollToElementJS(textBoxMenuItem);
+        click(textBoxMenuItem);
+        return new TextBoxPage();
     }
 
 
